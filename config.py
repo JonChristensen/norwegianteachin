1b2fa92951703d1a41782ef723cb1bb26b3b2b34
Jon Christensen <jon@kelsus.com>
2025-02-07 21:09:53 -0700

    forever fighting db config

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
     
-    # Define a local variable inside the class
-    _database_url = os.getenv("DATABASE_URL")
+    # Define a local variable inside the class for the database URL
+    _db_url = os.getenv("DATABASE_URL")
     
-    if _database_url and "localhost" not in _database_url:
-        SQLALCHEMY_DATABASE_URI = _database_url
+    if _db_url and "localhost" not in _db_url:
+        SQLALCHEMY_DATABASE_URI = _db_url
     else:
         SQLALCHEMY_DATABASE_URI = os.getenv("HEROKU_POSTGRESQL_RED_URL")
     
