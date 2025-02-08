5e53bb132b3253b56a2720263a951fb51516376b
Jon Christensen <jon@kelsus.com>
2025-02-07 21:04:37 -0700

    fighting db configuration

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
     
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
-    )
+    if DATABASE_URL and "localhost" not in DATABASE_URL:
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv("HEROKU_POSTGRESQL_RED_URL")
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
