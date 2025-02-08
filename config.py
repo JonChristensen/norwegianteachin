a7baba769421cbab27ec6389811b5510337f9120
Jon Christensen <jon@kelsus.com>
2025-02-07 21:19:47 -0700

    db hell

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
     
-    # Get the DATABASE_URL from the environment or fall back to HEROKU_POSTGRESQL_RED_URL
+    # Get the database URL from DATABASE_URL if set, otherwise from HEROKU_POSTGRESQL_RED_URL.
     _db_url = os.getenv("DATABASE_URL") or os.getenv("HEROKU_POSTGRESQL_RED_URL")
     
-    # If the URL starts with "postgres://", replace it with "postgresql://"
-    if _db_url and _db_url.startswith("postgres://"):
+    # If _db_url exists and starts with "postgres://", replace it with "postgresql://"
+    if _db_url:
         _db_url = _db_url.replace("postgres://", "postgresql://", 1)
     
     SQLALCHEMY_DATABASE_URI = _db_url
