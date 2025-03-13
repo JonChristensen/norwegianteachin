f40336451bab2939259a257bd62a31dfea46d5d8
Jon Christensen <jon@kelsus.com>
2025-03-13 08:59:45 -0600

    Update prisma seed configuration for ESM

--- nextjs_ver/norwegian-teachin-backup/next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // other config options
-};
-
-module.exports = nextConfig;
--- nextjs_ver/norwegian-teachin/app/exercise/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function ExerciseRedirect() {
-  redirect("/exercise/random")
-  return null
-} 
